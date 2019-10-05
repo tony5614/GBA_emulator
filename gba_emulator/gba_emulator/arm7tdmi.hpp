ba9b9bba811b176fe5541c9d6abf6635565c557d
tony5614 <gotony5614.me97@g2.nctu.edu.tw>
2019-10-05 15:24:25 +0800

    logical and physical memory map add MEMORY class add GBA_EMUALTOR_ARM7TDMI

--- gba_emulator/gba_emulator/arm7tdmi.hpp
+++ gba_emulator/gba_emulator/arm7tdmi.hpp
@@ -38,25 +38,37 @@ typedef unsigned int   U32;
 
 
 // General Internal Memory
-#define BIOS_BASE                            (0x00000000)
-#define BIOS_SIZE                            (0x00004000)
-#define ON_BOARD_WRAM_BASE                   (0x02000000)
-#define ON_BOARD_WRAM_SIZE                   (0x00040000)
-#define ON_CHIP_WRAM_BASE                    (0x03000000)
-#define ON_CHIP_WRAM_SIZE                    (0x00008000)
-#define IO_REGISTER_BASE                     (0x04000000)
-#define IO_REGISTER_SIZE                     (0x000003FF)
+#define BIOS_BASE_LOG                            (0x00000000)
+#define BIOS_SIZE                                (0x00004000)
+#define ON_BOARD_WRAM_BASE_LOG                   (0x02000000)
+#define ON_BOARD_WRAM_SIZE                       (0x00040000)
+#define ON_CHIP_WRAM_BASE_LOG                    (0x03000000)
+#define ON_CHIP_WRAM_SIZE                        (0x00008000)
+#define IO_REGISTER_BASE_LOG                     (0x04000000)
+#define IO_REGISTER_SIZE                         (0x00000400)   //actually (0x000003FF)
 // internal display memory
-#define PALETTE_RAM_BASE                     (0x05000000)
-#define PALETTE_RAM_SIZE                     (0x00000400)
-#define VIDEO_RAM_BASE                       (0x06000000)
-#define VIDEO_RAM_SIZE                       (0x00018000)
-#define OBJ_ATTR_RAM_BASE                    (0x07000000)
-#define OBJ_ATTR_RAM_SIZE                    (0x00000400)
+#define PALETTE_RAM_BASE_LOG                     (0x05000000)
+#define PALETTE_RAM_SIZE                         (0x00000400)
+#define VIDEO_RAM_BASE_LOG                       (0x06000000)
+#define VIDEO_RAM_SIZE                           (0x00018000)
+#define OBJ_ATTR_RAM_BASE_LOG                    (0x07000000)
+#define OBJ_ATTR_RAM_SIZE                        (0x00000400)
 //external memory(game pak)
-#define CARTRIDGE_ROM_WAIT_STATE_0_BASE      (0x08000000)
-#define CARTRIDGE_ROM_WAIT_STATE_0_SIZE      (0x01000000)
+#define CARTRIDGE_ROM_WAIT_STATE_0_BASE_LOG      (0x08000000)
+#define CARTRIDGE_ROM_WAIT_STATE_0_SIZE          (0x01000000)
 
+
+#define BIOS_BASE_PHY                            (0x00000000)
+#define ON_BOARD_WRAM_BASE_PHY                   (BIOS_BASE_PHY          + BIOS_SIZE)
+#define ON_CHIP_WRAM_BASE_PHY                    (ON_BOARD_WRAM_BASE_PHY + ON_BOARD_WRAM_SIZE)
+#define IO_REGISTER_BASE_PHY                     (ON_CHIP_WRAM_BASE_PHY  + ON_CHIP_WRAM_SIZE)
+#define PALETTE_RAM_BASE_PHY                     (IO_REGISTER_BASE_PHY   + IO_REGISTER_SIZE)
+#define VIDEO_RAM_BASE_PHY                       (PALETTE_RAM_BASE_PHY   + PALETTE_RAM_SIZE)
+#define OBJ_ATTR_RAM_BASE_PHY                    (VIDEO_RAM_BASE_PHY     + VIDEO_RAM_SIZE)
+#define CARTRIDGE_ROM_WAIT_STATE_0_BASE_PHY      (OBJ_ATTR_RAM_BASE_PHY  + OBJ_ATTR_RAM_SIZE)
+
+
+#define ALLOCATED_MEMORY_SIZE                (BIOS_SIZE + ON_BOARD_WRAM_SIZE + ON_CHIP_WRAM_SIZE + IO_REGISTER_SIZE + PALETTE_RAM_SIZE + VIDEO_RAM_SIZE + OBJ_ATTR_RAM_SIZE + CARTRIDGE_ROM_WAIT_STATE_0_SIZE)
 
 #define NUM_OF_REGISTER                      (16)
 #define USR_MODE                             (0x10)    // 10000b
@@ -89,8 +101,7 @@ typedef struct cartridge_rom_header
     U8  slave_id_number;
     U8  reserved3[26];
     U32 joybus_entry_point;
-}
-ROM_HEADER;
+}ROM_HEADER;
 
 typedef struct program_status_register 
 {
@@ -109,9 +120,52 @@ typedef struct program_status_register
 
 
 
-class ARM7TDMI
+class MEMORY 
 {
 public:
+    U8 raw_data[ALLOCATED_MEMORY_SIZE];
+    U8 operator[](U32 idx) 
+    {
+        //translate index
+        switch (idx & 0x0F000000)
+        {
+            case BIOS_BASE_LOG:
+                return raw_data[idx & 0x00FFFFFF + BIOS_BASE_PHY];
+                break;
+            case ON_BOARD_WRAM_BASE_LOG:
+                return raw_data[idx & 0x00FFFFFF + ON_BOARD_WRAM_BASE_PHY];
+                break;
+            case ON_CHIP_WRAM_BASE_LOG:
+                return raw_data[idx & 0x00FFFFFF + ON_CHIP_WRAM_BASE_PHY];
+                break;
+            case IO_REGISTER_BASE_LOG:
+                return raw_data[idx & 0x00FFFFFF + IO_REGISTER_BASE_PHY];
+                break;
+            case PALETTE_RAM_BASE_LOG:
+                return raw_data[idx & 0x00FFFFFF + PALETTE_RAM_BASE_PHY];
+                break;
+            case VIDEO_RAM_BASE_LOG:
+                return raw_data[idx & 0x00FFFFFF + VIDEO_RAM_BASE_PHY];
+                break;
+            case OBJ_ATTR_RAM_BASE_LOG:
+                return raw_data[idx & 0x00FFFFFF + OBJ_ATTR_RAM_BASE_PHY];
+                break;
+            case CARTRIDGE_ROM_WAIT_STATE_0_BASE_LOG:
+                return raw_data[idx & 0x00FFFFFF + CARTRIDGE_ROM_WAIT_STATE_0_BASE_PHY];
+                break;
+            default:
+                printf("unknown idx : %08X\n", idx);
+                return 0x00;
+                break;
+        }
+    }
+};
+
+
+class GBA_EMUALTOR_ARM7TDMI
+{
+public:
+    //cpu part
     //ARM state general egister and program counter
     //R13 : SP
     //R14 : LR
@@ -125,24 +179,23 @@ public:
     U32 R13_und, R14_und;
 
     //ARM state program status registers
-    CPSR CPSR_usr;      
+    CPSR CPSR_usr;
     SPSR SPSR_fiq;
     SPSR SPSR_svc;
     SPSR SPSR_abt;
     SPSR SPSR_irq;
     SPSR SPSR_und;
 
-
+    MEMORY   memory;
+    void readROM(std::string filename) 
+    {
+        std::ifstream fin;
+        fin.open(filename.c_str(), std::ios::binary | std::ios::in);
+        fin.read((char*)&memory.raw_data[CARTRIDGE_ROM_WAIT_STATE_0_BASE_PHY], CARTRIDGE_ROM_WAIT_STATE_0_SIZE);
+        fin.close();
+    }
     void run() 
     {
+        U8  bit_27_20 = memory[R[15]] & 0x0FF00000;
     }
-};
-
-
-
-
-class GBA_EMUALTOR 
-{
-public:
-    U8 memory[]
 };
